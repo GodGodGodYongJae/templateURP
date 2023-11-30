4ed69674492a36c19548f754d373edbe60a657ce
zhaqj12 <zhaqj12@naver.com>
2023-11-30 16:09:27 +0900

    초기 커밋.

---
+++ Assets/Scripts/GamePresenter.cs
@@ -0,0 +1,14 @@
+using VContainer;
+using VContainer.Unity;
+
+public class GamePresenter : ITickable
+{
+    readonly HelloWorldService helloWorldService;
+    public GamePresenter(HelloWorldService helloWorldService)
+    {
+        this.helloWorldService = helloWorldService;
+    }
+    void ITickable.Tick() {
+        helloWorldService.Hello();
+    }
+}
